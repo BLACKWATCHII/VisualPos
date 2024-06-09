82e149a221a1a3a794d8fff057dd867260f731ce
Kevin25DC <kevinhern49@gmail.com>
2024-06-09 16:50:37 -0500

    creation new app for customer

---
+++ customer/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    lastname = models.CharField(max_length=100, verbose_name="Apellido")
+    address = models.CharField(max_length=255, verbose_name="Dirección")
+    city = models.CharField(max_length=100, verbose_name="Ciudad")
+    cedula = models.CharField(max_length=20, verbose_name="Cédula")
+    phone = models.CharField(max_length=20, verbose_name="Teléfono")
+    email = models.EmailField(max_length=100, verbose_name="Correo electrónico")
+    record_date = models.DateField(verbose_name="Fecha de registro", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} {self.lastname}"
