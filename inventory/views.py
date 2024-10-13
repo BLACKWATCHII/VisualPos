517aeda3f1018201b8e3f3fbb2be0dfdc7c91a4c
Kevin25DC <kevinhern49@gmail.com>
2024-10-13 15:06:59 -0500

    add grafic sale to month

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Inventory
 
 # Create your views here.
+
+def view_inventory(request):
+    views = Inventory.objects.all()
+    print(views)
