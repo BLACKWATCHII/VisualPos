858ebaf01283715e7aaec9998d356d3f0dcd90ae
Kevin Hernandez :) <kevinhern49@gmail.com>
2025-04-27 08:10:32 -0500

    clear comment code

--- Apps/customer/urls.py
+++ Apps/customer/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # customer
     path('createCustomer/', views.Customer_create_view, name='createCustomer'),
     path('viewClient/', views.view_Clients, name='viewClient'),
     path('update_Customer/<int:client_id>/', views.update_Customer, name='update_Customer'),
@@ -10,5 +9,4 @@ urlpatterns = [
     path('export_clients_to_excel/', views.export_clients_to_excel, name='export_clients_to_excel'),
     path('cargar-datos-excel/', views.cargar_datos_excel, name='cargar_datos_excel'),
     path('clients/<int:client_id>/download/', views.download_pdf, name='download_pdf'),
-    #################################################################################### 
 ]
