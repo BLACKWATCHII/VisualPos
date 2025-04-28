1f457d4f1ea55de2860e87d298bc7387ede2067d
Kevin Hernandez :) <98708211+Kevin25DC@users.noreply.github.com>
2025-04-28 15:49:04 -0500

    new pag report invoice

--- Apps/Invoice/urls.py
+++ Apps/Invoice/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create-invoice/', views.create_invoice, name='create_invoice'),
+    path('report-invoice/',views.invoices_report,name='report_invoice'),
 ]
