0787b7c1320f34ea744cd75026ebe6e8711f9af7
Kevin Hernandez :) <98708211+Kevin25DC@users.noreply.github.com>
2025-05-26 15:05:23 -0500

    Update navbar logo size, enhance welcome message styling, and modify ngrok settings
    - Reduced the logo size in the navbar from 30px to 15px for better alignment.
    - Changed the welcome message color to green for improved visibility on the tasks page.
    - Updated ngrok URL in settings to reflect the new tunnel address for local development.

--- visualpos/settings.py
+++ visualpos/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-)w3(vn+@d#&ig&vt)edxpbjgwp&fcq@fla-)^)me)=)8)&&cbp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '73f8-152-201-164-96.ngrok-free.app'] # This is the ngrok URL and localhost (Tunnel)
-CSRF_TRUSTED_ORIGINS = ['https://73f8-152-201-164-96.ngrok-free.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ea05-2800-484-6c7a-11b0-98ff-9a5c-b93d-4e78.ngrok-free.app'] # This is the ngrok URL and localhost (Tunnel)
+CSRF_TRUSTED_ORIGINS = ['https://ea05-2800-484-6c7a-11b0-98ff-9a5c-b93d-4e78.ngrok-free.app']
 
 # Application definition
 
