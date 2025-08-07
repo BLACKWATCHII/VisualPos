e2727dffe73e6d585fe83276c48aaceadffacf8a
Kevin25DC <kevinhern49@gmail.com>
2025-08-07 08:12:41 -0500

    feat: Implement inventory adjustment feature with new view and template

--- Apps/inventory/views.py
+++ Apps/inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def ajustesInventario(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'Inventory/AjusteInventario.html')
